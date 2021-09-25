d1e7317b0dcf9506a0b191dcc7416cee1b93e3ea
tomito26 <tommybwah@gmail.com>
2021-09-25 20:12:30 +0300

    added a toggle function on double click of the task which adds a reminder class that adds green border on the left side of the task

--- src/App.js
+++ src/App.js
@@ -31,11 +31,16 @@ const deleteTask = (id)=>{
   setTasks(tasks.filter((task) => task.id !== id))
 }
 
+// Toggle reminder
+const toggleReminder = id =>{
+  setTasks(tasks.map(task=> task.id === id ? {...task,reminder:!task.reminder} : task ))
+}
+
   return (
     <div className="container">
       <Header  title="Task Tracker"/>
 
-      {tasks.length > 0 ? <Tasks tasks = {tasks} onDelete={deleteTask} /> : "No Tasks To Show"}
+      {tasks.length > 0 ? <Tasks tasks = {tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : "No Tasks To Show"}
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -2,12 +2,12 @@ import React from "react";
 import Task from "./Task";
 
 
-function Tasks({tasks,onDelete}) {
+function Tasks({tasks,onDelete,onToggle}) {
   
     return(
     <div>
         {tasks.map((task)=>(
-            <Task key={task.id} task={task} onDelete={onDelete}/>
+            <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle}/>
         ))}
     </div>
     );
